f20a4a02517511d47c980ee59a7cdbc0ab5a73af
ShirshakShahi <royalshirshal000@gmail.com>
2023-05-11 22:53:25 +0545

    using and understanding contextAPI

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-Here I am learning `useEffect()` , `Reducer` and `Context API` .
+Here I am learning `useEffect()` , `useReducer()` and `Context API` .
+
+
+
+`useEffect()`
+
+useEffect(()=>{
+    //some codes
+},[dependencies])
+
+useEffect runs when there is change in its dependencies, if no dependencies are provided the useEffect runs once
+
+`useReducer()`
+
+const[state,dispatchFn] = useReducer(reducerFn,initial value)
+
+const dispatchFn=(state,value)=>{
+    return updated state;
+}
+
+initial values like {value: "", property2 : ""}
+
+`contextAPI`
+
+my understanding is that this is used for the shortening the props and managing it by making a new file and storing objects there...
+and we can use
+
+context API like example here:
+
+    AuthContext = React.createContext({property:value})
+
+    <AuthContext.Provider value = {{
+        property: value
+    }} ></AuthContext.Provider>
+    
+    
+    <AuthContext.Consumer >
+        {(ctx)=>{
+            return jsx code goes here..
+        }}
+    </AuthContext.Consumer>
+
+
+alternative way is to use built-hook i.e`useContext()`
+
+const variable = useContext(ContextName);
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,6 +11,7 @@ const emailReducer = (state, action) => {
   if (action.type === "INPUT_BLUR") {
     return { value: state.value, isValid: state.value.includes('@') }
   }
+  return { value: " ", isValid: null }
 }
 
 const passwordReducer = (state, action) => {
@@ -20,6 +21,7 @@ const passwordReducer = (state, action) => {
   if (action.type === "INPUT_BLUR") {
     return { value: state.value, isValid: state.value.trim().length > 6 }
   }
+  return { value: " ", isValid: null }
 }
 
 const Login = (props) => {
@@ -28,22 +30,29 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: "", isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: "", isValid: null });
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid, passwordIsValid)
+
+    }, 500)
+
+    return () => { clearTimeout(identifier) };
+
+
+
+  }, [emailIsValid,passwordIsValid])
+
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value })
-
-
-    setFormIsValid(
-      event.target.value.includes('@') && passwordState.isValid
-    );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "USER_INPUT", val: event.target.value })
-
-
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
   };
 
   const validateEmailHandler = () => {
